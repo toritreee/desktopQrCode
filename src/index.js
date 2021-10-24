cde634ae8a0eb584cfdc206475af4dadf5cc63da
toritree <mitsunari.tori@gmail.com>
2021-10-24 11:57:48 +0900

    QRを読み取れるように

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import "./jsQR.js"
+class main{
+    constraints = { audio: false, video: true }
+
+    constructor(){
+        this.video = document.getElementById("video")
+        this.cv = document.createElement("canvas")
+        this.ctx = this.cv.getContext("2d")
+        this.getQrAnser = document.getElementById("getQrAnser")
+    }
+
+
+    media(){
+        this.mediaDevice = navigator.mediaDevices.getUserMedia(this.constraints)
+
+        this.mediaDevice.then((stream)=>{
+            this.video.srcObject = stream
+            video.onloadedmetadata = ()=>{
+                video.play()
+                this.checkImage()
+            }
+        })
+        .catch(this.mediaError)
+    }
+
+
+    mediaError(){}
+
+
+    checkImage(){
+        this.ctx.drawImage(this.video, 0, 0, this.cv.width, this.cv.height)
+        const imageData = this.ctx.getImageData(0, 0, this.cv.width, this.cv.height)
+
+        const code = jsQR(imageData.data, this.cv.width, this.cv.height)
+
+        if (code) {
+            this.getQr(code.data)
+        } else {
+            setTimeout(this.checkImage.bind(this), 200)
+        }
+    }
+    getQr(code){
+        document.getElementById("getQrAnser-data").value = code
+        document.getElementById("getQrAnser-open").onclick = ()=>window.location.href = code
+        document.getElementById("getQrAnser-copy").onclick = ()=>navigator.clipboard.writeText(code)
+        document.getElementById("getQrAnser-close").onclick = ()=>this.getQrAnser.close()
+        this.getQrAnser.showModal()
+        
+    }
+}
+
+
+new main().media()
---
+++ src/license.js
@@ -0,0 +1,10 @@
+function LICENSE(id){
+    let x = document.getElementById(id)
+    console.log(x)
+    x.showModal()
+}
+
+
+function RETURN(id){
+    document.getElementById(id).close()
+}
